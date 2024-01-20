ac31be23fe050c7187cb2e67d2f109d6fef51cda
Tahir <miantahir954@gmail.com>
2024-01-20 17:43:02 +0500

    day 1

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -22,6 +21,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -29,25 +29,56 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>  with TickerProviderStateMixin{
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   late AnimationController c1;
+
   @override
   void initState() {
-    c1=AnimationController(vsync: this);
+    c1 = AnimationController(vsync: this);
+
     super.initState();
   }
+
+  bool mannualT = false;
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black26,
       ),
       body: Column(
         children: [
-          ShakeY(
-            infinite: true,
+          SlideInLeft(
+            controller: (controller ) => c1=controller,
+            from: 200,
             animate: true,
-            child: const Text('Hey',),).centered(),
+            child: const Icon(
+              Icons.emoji_emotions_rounded,
+              size: 70,
+              color: Colors.yellow,
+            ).pOnly(top: 50).centered(),
+          ),
+          SlideInRight(
+            controller: ( controller ) => c1 = controller,
+            from: 200,
+            animate: true,
+            manualTrigger: mannualT,
+            child: const Icon(
+              Icons.emoji_emotions_rounded,
+              size: 70,
+              color: Colors.yellow,
+            ).pOnly(top: 50).centered(),
+          ),
+          MaterialButton(
+            onPressed: () {
+              setState(() {
+                mannualT=!mannualT;
+              });
+            },
+            child: Text('Start'),
+            color: Colors.red,
+          ),
         ],
       ),
     );
